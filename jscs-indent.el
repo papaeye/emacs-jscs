f864224a60f4b430379c0e592866a16dac05bc65
papaeye <papaeye@gmail.com>
2015-04-25 21:39:08 +0900

    Update Commentary

--- jscs-indent.el
+++ jscs-indent.el
@@ -23,9 +23,23 @@
 
 ;;; Commentary:
 
-;; (require 'jscs-indent)
-;; (add-hook 'js-mode-hook #'jscs-indent-apply)
-;; (add-hook 'js2-mode-hook #'jscs-indent-apply)
+;; jscs-indent applies JSCS indentation rules to JavaScript modes.
+;;
+;; Installation:
+;;
+;; 1. Install JSCS <http://jscs.info/>.
+;;
+;; 2. Put jscs-indent.el somewhere in your `load-path'.
+;;
+;; 3. Add the following code into your .emacs:
+;;
+;;     (with-eval-after-load 'js
+;;       (require 'jscs-indent)
+;;       (add-hook 'js-mode-hook #'jscs-indent-apply))
+;;
+;;     (with-eval-after-load 'js2-mode
+;;       (require 'jscs-indent)
+;;       (add-hook 'js2-mode-hook #'jscs-indent-apply))
 
 ;;; Code:
 
